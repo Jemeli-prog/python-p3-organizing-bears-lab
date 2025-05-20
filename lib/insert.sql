3f7729135ced9449c5a1af9bf787aa32ecee8299
Ruth Kiptoo <ruthkiptoo523@gmail.com>
2025-05-21 02:43:44 +0300

    sql

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 23, "F", "Brown", "Mad", "1");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 24, "M", "Black", "Happy", "0");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 27, "F", "Brown", "Mad", "1");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 24, "M", "Black", "Happy", "1");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 14, "F", "Brown", "Mad", "1");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 2, "M", "Green", "Happy", "0");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 13, "F", "Brown", "Mad", "0");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 24, "M", "Black", "Angry", "1");
+
+
+
